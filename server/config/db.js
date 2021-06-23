b17a5a9f757d3080ae7f2cce08cfd9c6f92518cb
Esen Yolcu <esenyolcu1986@gmail.com>
2021-06-23 00:08:50 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://esen:esen4821@mongodbserver.rwzmz.mongodb.net/books229?retryWrites=true&w=majority"
+};
